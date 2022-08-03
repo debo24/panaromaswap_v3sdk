e241ec3989aa4fa2ead77702e45783215901a68f
debo24 <71261693+debo24@users.noreply.github.com>
2022-08-03 20:10:38 +0530

    factory updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const FACTORY_ADDRESS = '0xe4Ea5f13957244A1c69379843E6df404B71C4d62'
+export const FACTORY_ADDRESS = '0xcBd5a2b15d20BdF4c7d6e2B692ABc1D71ccb3bdd'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH = '0xecd681bb74a2488c612a04ee2aef3162d6305089892ba2639b666665dad5cd3d'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
