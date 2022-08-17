2f3fe9d0765d2df5e55601e6323e9fdf9c2162ea
debo24 <71261693+debo24@users.noreply.github.com>
2022-08-17 20:12:37 +0530

    POOL_INIT_CODE_HASH updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,11 @@
+import IPanaromaswapV3Pool from 'panaromaswap_v3core/artifacts/contracts/PanaromaswapV3Pool.sol/PanaromaswapV3Pool.json'
+import { keccak256 } from '@ethersproject/solidity'
+
 export const FACTORY_ADDRESS = '0x89538C05630E6a6128f36A902b8a6217C17181EA'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xecd681bb74a2488c612a04ee2aef3162d6305089892ba2639b666665dad5cd3d'
+export const POOL_INIT_CODE_HASH = keccak256(['bytes'], [IPanaromaswapV3Pool.bytecode])
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
