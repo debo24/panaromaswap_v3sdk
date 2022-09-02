c53c5b0660e033c780f91f6c47adb3fe3847a76f
debo24 <71261693+debo24@users.noreply.github.com>
2022-09-02 16:01:29 +0530

    POOL_INIT_CODE_HASH updated

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const FACTORY_ADDRESS = '0x89538C05630E6a6128f36A902b8a6217C17181EA'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xecd681bb74a2488c612a04ee2aef3162d6305089892ba2639b666665dad5cd3d'
+export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
